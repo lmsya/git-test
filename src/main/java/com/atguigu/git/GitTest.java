f823c2ec2610845bda2cdd710fde98b536f76909
lzm <lzm@qq.com>
2022-10-28 16:01:38 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * Date: 2022/10/28 15:56
+ * Author: lzm
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
